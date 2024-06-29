deefa4c69f22edb69f431fb672d4d184a5781e8f
LillaDou <lilla.e.j.du@gmail.com>
2024-06-29 21:01:27 +0200

    Eliminar un Todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -39,11 +39,21 @@ export const App = ( elementId ) => {
         todoStore.addTodo(event.target.value);
         displayTodos();
         event.target.value = '';
-    })
+    });
 
     todoListUL.addEventListener('click', (event) => {
         const element = event.target.closest('[data-id]');
         todoStore.toggleTodo( element.getAttribute('data-id'));
         displayTodos();
-    })
+    });
+
+    todoListUL.addEventListener('click', (event) => {
+        const isDestroyElement = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+
+        if( !element || !isDestroyElement) return;
+
+        todoStore.deleteTodo( element.getAttribute('data-id'));
+        displayTodos();
+    });
 }
