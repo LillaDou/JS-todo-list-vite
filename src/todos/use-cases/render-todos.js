96ce0ea56b3c90f437fb6803f493957faba7f35a
LillaDou <lilla.e.j.du@gmail.com>
2024-06-29 20:29:02 +0200

    Validaciones y optimizaciones

--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from '../models/todo.model';
 import { createTodoHTML } from './create-todo-html';
 
+let element;
+
 
 /**
  * 
@@ -9,7 +11,12 @@ import { createTodoHTML } from './create-todo-html';
  */
 export const renderTodos = ( elementId, todos = [] ) => {
 
-    const element = document.querySelector( elementId );
+    if(!element )
+        element = document.querySelector( elementId );
+
+    if( !element ) throw new Error(`Element ${ elementId } not found`);
+
+    element.innerHTML = '';
 
     todos.forEach(todo => {
         element.append( createTodoHTML(todo) );
