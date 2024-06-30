b57916d33611944429d87a46aa320407c378b409
LillaDou <lilla.e.j.du@gmail.com>
2024-06-30 12:01:55 +0200

    Mostrar la cantidad de TODOs pendientes

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,13 +1,14 @@
 import html from './app.html?raw';
 import todoStore, { Filters } from '../store/todo.store';
-import { renderTodos } from './use-cases';
+import { renderTodos, renderPending } from './use-cases';
 
-
+//Referencias para mantener nuestros strings
 const ElementIds = {
     ClearCompletedButton: '.clear-completed',
     TodoList: '.todo-list',
     NewTodoInput: '#new-todo-input',
     TodoFilters: '.filtro',
+    PendingCountLabel: '#pending-count',
 }
 /**
  * 
@@ -19,6 +20,11 @@ export const App = ( elementId ) => {
     const displayTodos = () => {
         const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
         renderTodos(ElementIds.TodoList, todos );
+        updatePendingCount();
+    };
+
+    const updatePendingCount = () => {
+        renderPending(ElementIds.PendingCountLabel);
     }
 
     //Cuando la función App() se llama
